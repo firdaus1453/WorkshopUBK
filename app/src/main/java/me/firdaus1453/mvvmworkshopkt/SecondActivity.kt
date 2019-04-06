43fd632595d5c5c9709263992da5f876417f7cf7
firdaus1453 <mfirdaus1453@gmail.com>
2019-04-06 22:02:57 +0800

    Initial commit

---
+++ app/src/main/java/me/firdaus1453/mvvmworkshopkt/SecondActivity.kt
@@ -0,0 +1,12 @@
+package me.firdaus1453.mvvmworkshopkt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
